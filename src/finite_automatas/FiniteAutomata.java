a7baf0785e296218e450ecc926b0971e796405ad
VPBLIN <blin@veritasprep.com>
2015-05-08 12:08:59 -0700

    finite automata

--- src/finite_automatas/FiniteAutomata.java
+++ src/finite_automatas/FiniteAutomata.java
@@ -1,5 +1,69 @@
 package finite_automatas;
 
-public class FiniteAutomata {
+import java.util.Scanner;
+import java.lang.String;
 
-}
+public class FiniteAutomata {
+//thanks http://stackoverflow.com/questions/20153970/how-do-i-implement-a-dfa-in-java-without-using-reg-expression
+    public static void main(String[] args) throws Exception {
+
+        //create the file instance
+        java.io.File file = new java.io.File("DFA.txt");
+        //it's scanner time
+        Scanner input = new Scanner(file);
+        //read it
+        int dfaAmount = input.nextInt();
+        //loop for amount of dfas
+       for (int dfaAmountIndex = 0; dfaAmountIndex < dfaAmount;
+                dfaAmountIndex++){
+            int numberOfStates = input.nextInt();
+            int startState;
+            int numberOfSigmaSym;
+
+
+
+            String sigma = "";
+            sigma = input.next();
+
+
+            numberOfSigmaSym = sigma.length();
+            String transitionLine = input.nextLine();
+            int[][] transition = new int[numberOfStates][numberOfSigmaSym];
+
+
+            startState = input.nextInt();    
+            //checker
+            System.out.println(startState);
+            for (int i=0; i < numberOfStates; i++){
+            	int j = 0;
+                numberOfStates = i;
+                numberOfSigmaSym = j;
+                for (j=0; j < sigma.length(); j++){
+            int state1 = transition[i][j];
+                }
+        }
+
+            String w;
+            w = input.nextLine();
+            for (int i=0; i < w.length(); i++){
+
+                 char x;
+               x = w.charAt(i);
+               int state = startState;
+                int index = sigma.indexOf(x);
+                int state1 = transition [state][index];
+            }
+            int numberOfAcceptStates;
+            numberOfAcceptStates = input.nextInt();
+            int acceptState;
+            acceptState = input.nextInt();
+
+
+
+
+
+
+          }
+       }
+
+    }
---
+++ src/finite_automatas/FiniteAutomata_Assignment11.java
@@ -0,0 +1,7 @@
+package finite_automatas;
+
+public class FiniteAutomata_Assignment11 {
+	public static void main(String args[]){
+		
+	}
+}
