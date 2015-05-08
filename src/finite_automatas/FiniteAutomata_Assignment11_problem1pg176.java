a24f8678938ae105b080b8f5bbb23f6182e8cc74
VPBLIN <blin@veritasprep.com>
2015-05-08 13:17:56 -0700

    all hw

--- src/finite_automatas/FiniteAutomata_Assignment11_problem1pg176.java
+++ src/finite_automatas/FiniteAutomata_Assignment11_problem1pg176.java
@@ -2,14 +2,15 @@ package finite_automatas;
 
 import java.util.Scanner;
 
-public class FiniteAutomata_Assignment11 {
+public class FiniteAutomata_Assignment11_problem1pg176 {
 	public static void main(String args[]){
-		int state = 1;
 		
 		Scanner in = new Scanner(System.in);
 		for(int i = 0; i < 100; i ++){
+			int state = 1;
+
 			System.out.println("String automata");
-			int current_char = 0;
+			int current_char = 1;
 			String input = in.next();
 
 		while(current_char < input.length()){
@@ -22,9 +23,6 @@ public class FiniteAutomata_Assignment11 {
 						case 'a':
 							state = 2;
 						break;
-						case 'c':
-							state = 6;
-						break;
 					}
 				break;
 				case 2:
@@ -35,73 +33,39 @@ public class FiniteAutomata_Assignment11 {
 						case 'b':
 							state = 4;
 						break;
-						case 'c':
-							state = 2;
-						break;
 					}
 				break;
 				case 3:
 					switch(input_char){
 						case 'a':
-							state = 4;
+							state = 3;
 						break;
 						case 'b':
-							state = 2;
+							state = 4;
 						break;
-						case 'c':
-							state = 2;
-						break;
-
 					}
 				break;
 				case 4:
 					switch(input_char){
-						case 'a':
-							state = 3;
-						break;
 						case 'b':
-							state = 1;
-						break;
-						case 'c':
 							state = 5;
 						break;
-
 					}
 				break;
 				case 5:
 					switch(input_char){
 						case 'a':
-							state = 1;
-						break;
-						case 'b':
 							state = 4;
 						break;
-						case 'c':
-							state = 6;
-						break;
-
 					}
 				break;
-				case 6:
-					switch(input_char){
-						case 'a':
-							state = 1;
-						break;
-						case 'b':
-							state = 5;
-						break;
-						case 'c':
-							state = 4;
-						break;
-
-					}
-				break;
-
 
 			}
+			System.out.println(input_char + " "+ state);
+
 		}
 		
-		if(state ==6){
+		if(state ==1){
 			System.out.println(input + "Accepted. End state was " + state);
 			
 		}else{
